9e87e9b826d8e14f1dfdf0e463750dc098f2fdd1
speechless29 <nguyentrongtin291209@gmail.com>
2025-12-11 17:20:51 +0700

    Create multi pages using react router

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState,useRef,UseEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import {Routes,Route} from 'react-router'
 
 function App() {
   return (
-    <>
-      <HomePage/>
-    </>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='checkout' element={<div>Checkout Page</div>}/>
+      </Routes>
   )
 }
 

4556c79fc6bcdbe251b992d0e10c72203a7c44e9
speechless29 <nguyentrongtin291209@gmail.com>
2025-12-11 17:40:15 +0700

    Move orders page into react

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { Routes, Route } from "react-router";
 import { CheckoutPage } from "./pages/CheckoutPage";
-
+import { OrdersPage } from "./pages/OrdersPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
 }

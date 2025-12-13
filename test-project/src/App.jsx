7b6f2be4cdb1550bacd77f4ff547142efbcd5d48
speechless29 <nguyentrongtin291209@gmail.com>
2025-12-13 16:36:57 +0700

    Generate HTML for Orders Page

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrdersPage />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
   );
 }
